d65dc2d6ce632084bf465e34454dce4c015c071e
Leonard Perez Santana <lperez@confisa.do>
2022-06-20 20:15:56 -0400

    Factory Pattern sample for Jhon

---
+++ FactorySample/FactoryMathOpr.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorySample
+{
+    //Creator
+    public abstract class FactoryMathOpr
+    {
+        public abstract IMathOpr OprExec();
+    }
+
+    //Concret Creator
+    public class AdditionOprFactory : FactoryMathOpr
+    {
+        private decimal _valor1;
+        private decimal _valor2;
+
+        public AdditionOprFactory(decimal valor1, decimal valor2)
+        {
+            _valor1 = valor1;
+            _valor2 = valor2;            
+        }
+
+        public override IMathOpr OprExec()
+        {
+            return new AdditionOpr(_valor1, _valor2);
+        }
+    }
+
+
+    //Concret Product
+    public class AdditionOpr : IMathOpr
+    {
+        private decimal _valor1;
+        private decimal _valor2;
+
+        public AdditionOpr(decimal valor1, decimal valor2)
+        {
+            _valor1 = valor1;
+            _valor2 = valor2;
+        }
+
+        public decimal OprExec(decimal valor1, decimal valor2)
+        {
+            return valor1 + valor2;
+        }
+    }
+
+    //Product
+    public interface IMathOpr
+    {
+        public decimal OprExec(decimal valor1, decimal valor2);
+    }
+
+   
+}
---
+++ FactorySample/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FactorySample;
+
+Console.WriteLine("Hello, World!");
+
+FactoryMathOpr Addition = new AdditionOprFactory(5, 7);
+
+IMathOpr Add = Addition.OprExec();
+
+decimal resultado = Add.OprExec(5, 2);
+
+Console.WriteLine($"El resultado de la suma es {resultado}");
+
+Console.ReadLine();
